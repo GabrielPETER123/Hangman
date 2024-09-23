a49d5903102d9f7cd996151fd12f4376c3a05df2
Gabriel PETER <gabriel.peter@ynov.com>
2024-09-23 15:36:46 +0200

    Refactor hangman game logic and input handling

--- hangman.go
+++ hangman.go
@@ -10,8 +10,11 @@ import (
 
 func main() {
 	var mot string
-	var boolInput bool
+	var int_input int
 	ListInput := []string{}
+	find_mot := false
+	bad_guesses := 0
+	attempts := 10
 	//	fichier, err := os.Open("words.txt")
 	//	if err != nil {
 	//		fmt.Printf("The error is: %v", err.Error())
@@ -22,52 +25,59 @@ func main() {
 	// compter le nombre de lignes/mots avec le scanner et la mettre dans une variable à utiliser pour la variable aléatoire "nMot"
 	// variable aléatoire "nMot" pour choisir un mot
 	// variable pour la ligne (qui lit le mot)
-
-	//	attempts := 10
-
-	//lire la sortie standard
-	reader := bufio.NewReader(os.Stdin)
-	inputLettre, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	//	if attempts == 10 {
-	//		func(len mot)
-	//	}
-
-	//message d'erreur
-	if len(inputLettre) > 3 {
-		fmt.Print("Too much input")
-	}
-	if len(inputLettre) < 3 {
-		fmt.Print("Not enough input")
-	} else {
-		boolInput, ListInput = VerifyInput(inputLettre, mot, ListInput)
-		if boolInput {
-			fmt.Print("Bien joué!")
+	for attempts > 0 || find_mot != true {
+		input := ReadInput()
+		if len(input) > 1 {
+			fmt.Print("Too much input")
+		}
+		if len(input) < 1 {
+			fmt.Print("Not enough input")
 		} else {
-			fmt.Print("Mauvaise réponse")
+			attempts--
+			int_input, ListInput = VerifyInput(input, mot, ListInput)
+			if int_input == 1 {
+				fmt.Print("Bien joué!\n")
+			} else if int_input == 0 {
+				fmt.Print("Mauvaise réponse\n")
+				bad_guesses++
+				PrintHangman(bad_guesses)
+			}
 		}
 	}
+	return
 }
 
-func PrintHangman() {
+func PrintHangman(bad_guesses int) {
+	hangman_file, err := os.Open("hangman.txt")
+	if err != nil {
+		fmt.Printf("The error is: %v", err.Error())
+		return
+	}
+	defer hangman_file.Close()
+	hangman_state := bufio.NewScanner(hangman_file)
+	for hangman_state.Scan() {
+	}
 }
 
-func Intn(n int) int {
+func RandomNbr(n int) int {
 	return rand.Intn(n)
 }
 
-func VerifyInput(s string, mot string, ListInput []string) (bool, []string) {
+func VerifyInput(s string, mot string, ListInput []string) (int, []string) {
 	if strings.Contains(strings.Join(ListInput, ""), s) {
-		fmt.Println("Lettre déjà utilisée.")
-		return false, ListInput
+		fmt.Print("Lettre déjà utilisée.\n")
+		return -1, ListInput
 	}
 	runeS := []rune(s)[0]
 	if strings.ContainsRune(mot, runeS) {
-		return true, append(ListInput, s)
+		return 1, append(ListInput, s)
 	} else {
-		return false, append(ListInput, s)
+		return 0, append(ListInput, s)
 	}
+}
+
+func ReadInput() string {
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	return input.Text()
 }
