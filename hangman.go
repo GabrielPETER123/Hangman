82c167d69cf12dcea04e880d94db537ee090a1ea
Gabriel PETER <gabriel.peter@ynov.com>
2024-09-25 12:55:24 +0200

    Refactor hangman game logic and input handling

--- hangman.go
+++ hangman.go
@@ -15,47 +15,91 @@ func main() {
 	find_mot := false
 	bad_guesses := 0
 	attempts := 10
-	//	fichier, err := os.Open("words.txt")
-	//	if err != nil {
-	//		fmt.Printf("The error is: %v", err.Error())
-	//		return
-	//	}
-	//	defer fichier.Close()
-	//	scanner := bufio.NewScanner(fichier)
-	// compter le nombre de lignes/mots avec le scanner et la mettre dans une variable à utiliser pour la variable aléatoire "nMot"
-	// variable aléatoire "nMot" pour choisir un mot
-	// variable pour la ligne (qui lit le mot)
-	for attempts > 0 || find_mot != true {
+	fichier, err := os.Open("words.txt")
+	if err != nil {
+		fmt.Printf("The error is: %v", err.Error())
+		return
+	}
+	defer fichier.Close()
+	scan := bufio.NewScanner(fichier)
+	for scan.Scan() {
+		mot = scan.Text()
+		find_mot = true
+	}
+	if find_mot == false {
+		fmt.Print("No word found\n")
+		return
+	}
+	fmt.Print("Bienvenue dans le jeu du pendu!\nÉcrivez une lettre pour essayer de deviner le mot!\n")
+	for attempts > 0 && find_mot == true {
 		input := ReadInput()
 		if len(input) > 1 {
-			fmt.Print("Too much input")
+			fmt.Print("Too much input\n")
 		}
 		if len(input) < 1 {
-			fmt.Print("Not enough input")
+			fmt.Print("Not enough input\n")
 		} else {
-			attempts--
+			if VerifyWord(mot, ListInput) {
+				fmt.Print("Vous avez déjà trouvé le mot!\n")
+				return
+			}
 			int_input, ListInput = VerifyInput(input, mot, ListInput)
-			if int_input == 1 {
-				fmt.Print("Bien joué!\n")
+			if int_input == -1 {
+				continue
 			} else if int_input == 0 {
-				fmt.Print("Mauvaise réponse\n")
 				bad_guesses++
 				PrintHangman(bad_guesses)
+				fmt.Printf("Nombre de tentatives restantes: %d\n", attempts-bad_guesses)
+			}
+			PrintMot(mot, ListInput)
+			if strings.Contains(mot, strings.Join(ListInput, "")) {
+				fmt.Print("Bien joué!\n")
+				break
+			} else if bad_guesses == attempts {
+				fmt.Print("Pas de chance... Mauvaise réponse !\n")
+				break
 			}
 		}
 	}
 	return
 }
 
+func PrintMot(mot string, ListInput []string) {
+	nbr_letter_reveal := (len(mot) / 2) - 1
+	for i := 0; i < nbr_letter_reveal; i++ {
+		rand_letter := RandomNbr(len(mot))
+		if strings.Contains(strings.Join(ListInput, ""), string(mot[rand_letter])) {
+			continue
+		} else {
+			ListInput = append(ListInput, string(mot[rand_letter]))
+		}
+	}
+	for _, letter := range mot {
+		if strings.Contains(strings.Join(ListInput, ""), string(letter)) {
+			fmt.Print(string(letter))
+		} else {
+			fmt.Print("_")
+		}
+	}
+	fmt.Print("\n")
+}
+
 func PrintHangman(bad_guesses int) {
+	start := 1 * bad_guesses
+	end := 7*bad_guesses + start
+	read_line := 1
 	hangman_file, err := os.Open("hangman.txt")
 	if err != nil {
 		fmt.Printf("The error is: %v", err.Error())
 		return
 	}
 	defer hangman_file.Close()
-	hangman_state := bufio.NewScanner(hangman_file)
-	for hangman_state.Scan() {
+	hangman_scan := bufio.NewScanner(hangman_file)
+	for hangman_scan.Scan() {
+		if lineread := hangman_scan.Text(); read_line >= start && read_line <= end {
+			fmt.Println(lineread)
+		}
+		read_line++
 	}
 }
 
@@ -80,4 +124,13 @@ func ReadInput() string {
 	input := bufio.NewScanner(os.Stdin)
 	input.Scan()
 	return input.Text()
+}
+
+func VerifyWord(mot string, ListInput []string) bool {
+	var char_word []string
+	for _, letter := range mot {
+		if char_word == rune 
+		char_word = append(char_word, string(letter))
+	}
+
 }
